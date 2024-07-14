65c1b82001dc88068e037e27842727721cfece40
kap-taan <harshsukhijacse@gmail.com>
2024-07-14 19:11:41 +0530

    Initial commit

---
+++ src/main/java/com/docdrop/dropengine/DropengineApplication.java
@@ -0,0 +1,13 @@
+package com.docdrop.dropengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docdrop/dropengine/DropengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docdrop.dropengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
