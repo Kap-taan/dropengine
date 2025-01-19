e2dde84cd9e741c8cebcc8258caf66cbe71cb7be
kap-taan <harshsukhijacse@gmail.com>
2025-01-20 00:26:45 +0530

    changed the ErrorResponse.java file

--- src/main/java/com/hsvd/notesapp/entity/ErrorResponse.java
+++ src/main/java/com/hsvd/notesapp/entity/ErrorResponse.java
@@ -13,7 +13,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ErrorResponse {
-    
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime timeStamp;
     private List<String> messages;
